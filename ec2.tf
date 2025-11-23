00382bd482252e68321e1fd9ccf45ff8e75657ee
glen chua <glenchua91@hotmail.com>
2025-11-24 00:10:42 +0800

    correct ec2 user data

--- ec2.tf
+++ ec2.tf
@@ -6,25 +6,27 @@ resource "aws_instance" "fastAPI" {
   subnet_id                   = aws_subnet.l2c-web-a.id
   key_name                    = aws_key_pair.ssh_key.key_name
 
-  user_data = <<EOF
+  user_data = <<-EOF
     #!/bin/bash
     sleep 60
 
-    cd journal-starter/
-    source .env
-    sed -i "s|DATABASE_URL=.*$|DATABASE_URL=postgresql://postgres:$$POSTGRES_PASSWORD@$(aws ec2 describe-instances \
+    cd /home/ec2-user/journal-starter
+    source .env 
+    sed -i "s|DATABASE_URL=.*$|DATABASE_URL=postgresql://postgres:$${POSTGRES_PASSWORD}@$(aws ec2 describe-instances \
     --filters \
     "Name=tag:Name,Values=Db server" \
     --query 'Reservations[*].Instances[*].PrivateIpAddress' \
     --output text | tr -d ' '):5432/career_journal|" .env
 
-    sed -i "s/server_name.*$$/server_name $(aws ec2 describe-instances \
+    sed -i "s/server_name.*$/server_name $(aws ec2 describe-instances \
     --filters \
     "Name=tag:Name,Values=Db server" \
     --query 'Reservations[*].Instances[*].PrivateIpAddress' \
-    --output text | tr -d ' ')/" /etc/nginx/conf.d/fastapi_nginx.conf
+    --output text | tr -d ' ');/" /etc/nginx/conf.d/fastapi_nginx.conf
 
     service nginx restart
+
+    ./start.sh
   EOF
 
 
