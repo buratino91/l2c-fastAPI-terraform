9744c4c4a22b05c4e3287bf3437b245d0b055743
glen chua <glenchua91@hotmail.com>
2025-12-02 16:29:15 +0800

    remove SSM resource policy

--- modules/Security/IAM.tf
+++ modules/Security/IAM.tf
@@ -30,64 +30,6 @@ resource "aws_iam_role" "ec2-describe-role" {
   })
 }
 
-# resource "aws_iam_policy" "SSMManagedInstanceCore" {
-#   name = "SSMCore"
-#   description = "policy for managing ec2 in SSM"
-
-#   policy = <<EOT
-#   {
-#   "Version" : "2012-10-17",
-#   "Statement" : [
-#     {
-#       "Effect" : "Allow",
-#       "Action" : [
-#         "ssm:DescribeAssociation",
-#         "ssm:GetDeployablePatchSnapshotForInstance",
-#         "ssm:GetDocument",
-#         "ssm:DescribeDocument",
-#         "ssm:GetManifest",
-#         "ssm:GetParameter",
-#         "ssm:GetParameters",
-#         "ssm:ListAssociations",
-#         "ssm:ListInstanceAssociations",
-#         "ssm:PutInventory",
-#         "ssm:PutComplianceItems",
-#         "ssm:PutConfigurePackageResult",
-#         "ssm:UpdateAssociationStatus",
-#         "ssm:UpdateInstanceAssociationStatus",
-#         "ssm:UpdateInstanceInformation"
-#       ],
-#       "Resource" : "*"
-#     },
-#     {
-#       "Effect" : "Allow",
-#       "Action" : [
-#         "ssmmessages:CreateControlChannel",
-#         "ssmmessages:CreateDataChannel",
-#         "ssmmessages:OpenControlChannel",
-#         "ssmmessages:OpenDataChannel"
-#       ],
-#       "Resource" : "*"
-#     },
-#     {
-#       "Effect" : "Allow",
-#       "Action" : [
-#         "ec2messages:AcknowledgeMessage",
-#         "ec2messages:DeleteMessage",
-#         "ec2messages:FailMessage",
-#         "ec2messages:GetEndpoint",
-#         "ec2messages:GetMessages",
-#         "ec2messages:SendReply"
-#       ],
-#       "Resource" : "*"
-#     }
-#   ]
-# }
-# EOT
-# }
-
-
-
 resource "aws_iam_role_policy_attachment" "SSM_Role-attach" {
   role       = aws_iam_role.SSM_ec2_Role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
